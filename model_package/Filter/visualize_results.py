f9cc2e8c47446cd7ecec259ef468990c9e632acc
Thomas Ernest Allard <tea@lanl.gov>
2024-09-20 16:13:30 -0600

    FEAT: add new stress norm plotting

--- model_package/Filter/visualize_results.py
+++ model_package/Filter/visualize_results.py
@@ -496,6 +496,97 @@ def plot_stress_norm(cauchy, symm, m, nqp, nel, ninc, times, output_name):
     return 0
 
 
+def triple_deviatoric(stress):
+    '''Calculate the deviatoric component of a stress quantity
+
+    :param array-like stress: A second order tensor or 9-component slice of a third order tensor
+
+    :returns: deviatoric of ``stress``
+    '''
+
+    stress_1 = stress[:,:,0] - (1/3)*numpy.trace(stress[:,:,0])
+    stress_2 = stress[:,:,1] - (1/3)*numpy.trace(stress[:,:,1])
+    stress_3 = stress[:,:,2] - (1/3)*numpy.trace(stress[:,:,2])
+
+    return(stress_1 + stress_2 + stress_3)
+
+
+def plot_better_stress_norm(PK2, SIGMA, M, E, Ecal, Gamma, nqp, nel, ninc, output_name):
+    '''Plot the infinity norms of deviatoric Cauchy, symmetric micro, and higher stresses
+
+    :param dict cauchy: The quantities dict storing Cauchy stress
+    :param dict symm: The quantities dict storing symmetric micro stress
+    :param dict m: THe quantities dict storing higher order stress
+    :param int nqp: The number of quadrature points
+    :param int nel: The number of elements
+    :param array-like times: The time increments
+    :param str output_name: Output filename
+
+    :returns: ``output_name`` plot
+    '''
+
+    #fig = plt.figure('stress_norms', figsize=(16,12), dpi=300)
+    #axes = fig.add_subplot(4, 3, 12)
+    #axes = [[fig.add_subplot(4,3,3 * i + j + 1) for j in range(3)] for i in range(4)]
+    #axes = [[fig.add_subplot(4,3,12) for j in range(3)] for i in range(4)]
+    fig, axes = plt.subplots(4, 3)
+
+    for el in range(nel):
+        for qp in range(nqp):
+            # take norms
+            PK2_norm, SIGMA_norm, diff_norm = [], [], []
+            M1_norm, M2_norm, M3_norm = [], [], []
+            E_norm, Ecal_norm, Gamma_norm = [], [], []
+            for t in range(ninc):
+                PK2_norm.append(numpy.linalg.norm(deviatoric(PK2[qp][t,el,:,:]), ord='fro'))
+                SIGMA_norm.append(numpy.linalg.norm(deviatoric(SIGMA[qp][t,el,:,:]), ord='fro'))
+                diff_norm.append(numpy.linalg.norm(deviatoric(PK2[qp][t,el,:,:]-SIGMA[qp][t,el,:,:]), ord='fro'))
+                M1_norm.append(numpy.linalg.norm(deviatoric(M[qp][t,el,:,:,0]), ord='fro'))
+                M2_norm.append(numpy.linalg.norm(deviatoric(M[qp][t,el,:,:,1]), ord='fro'))
+                M3_norm.append(numpy.linalg.norm(deviatoric(M[qp][t,el,:,:,2]), ord='fro'))
+                E_norm.append(numpy.linalg.norm(deviatoric(E[qp][t,el,:,:]), ord='fro'))
+                Ecal_norm.append(numpy.linalg.norm(deviatoric(Ecal[qp][t,el,:,:]), ord='fro'))
+                Gamma_norm.append(numpy.linalg.norm(triple_deviatoric(Gamma[qp][t,el,:,:,:]), ord='fro'))
+
+            label = f"qp  #{(qp+1)+(nel*8)}"
+
+            axes[0][0].plot(E_norm, PK2_norm)
+            axes[1][0].plot(E_norm, SIGMA_norm)
+            axes[2][0].plot(E_norm, diff_norm)
+            axes[3][0].plot(E_norm, M1_norm, '-o', label=f"index K = 1, {label}")
+            axes[3][0].plot(E_norm, M2_norm, '-^', label=f"index K = 2, {label}")
+            axes[3][0].plot(E_norm, M3_norm, '-v', label=f"index K = 3, {label}")
+
+            axes[0][1].plot(Ecal_norm, PK2_norm)
+            axes[1][1].plot(Ecal_norm, SIGMA_norm)
+            axes[2][1].plot(Ecal_norm, diff_norm)
+            axes[3][1].plot(Ecal_norm, M1_norm, '-o', label=f"index K = 1, {label}")
+            axes[3][1].plot(Ecal_norm, M2_norm, '-^', label=f"index K = 2, {label}")
+            axes[3][1].plot(Ecal_norm, M3_norm, '-v', label=f"index K = 3, {label}")
+
+            axes[0][2].plot(Gamma_norm, PK2_norm)
+            axes[1][2].plot(Gamma_norm, SIGMA_norm)
+            axes[2][2].plot(Gamma_norm, diff_norm)
+            axes[3][2].plot(Gamma_norm, M1_norm, '-o', label=f"index K = 1, {label}")
+            axes[3][2].plot(Gamma_norm, M2_norm, '-^', label=f"index K = 2, {label}")
+            axes[3][2].plot(Gamma_norm, M3_norm, '-v', label=f"index K = 3, {label}")
+
+    axes[0][0].set_ylabel(r'$||dev\left(S_{IJ}\right)|| \left( MPa \right)$', fontsize=14)
+    axes[1][0].set_ylabel(r'$||dev\left(\Sigma_{IJ}\right)|| \left( MPa \right)$', fontsize=14)
+    axes[2][0].set_ylabel(r'$||dev\left(S_{IJ} - \Sigma_{IJ}\right)|| \left( MPa \right)$', fontsize=14)
+    axes[3][0].set_ylabel(r'$||dev\left(M_{IJK}\right)|| \left( MPa \cdot mm^2 \right)$', fontsize=14)
+    axes[3][0].set_xlabel(r'$||dev\left(E_{IJ}\right)|| \left( MPa \right)$', fontsize=14)
+    axes[3][1].set_xlabel(r'$||dev\left(\mathcal{E}_{IJ}\right)|| \left( MPa \right)$', fontsize=14)
+    axes[3][2].set_xlabel(r'$||dev\left(\Gamma_{IJK}\right)|| \left( MPa \right)$', fontsize=14)
+
+    fig.set_figheight(16)
+    fig.set_figwidth(12)
+    fig.tight_layout()
+    fig.savefig(output_name)
+
+    return 0
+
+
 def csv_machine(quantity, output_file, nqp, nel, time, three_point=False):
     '''Write a text dump summarizing a variety of statistics
 
@@ -679,6 +770,7 @@ def visualize_results(input_file, average, num_domains,
                       plot_rotation_diff=None,
                       plot_stretch_diff=None,
                       plot_stress_norms=None,
+                      plot_better_stress_norms=None,
                       csv_cauchy=None,
                       csv_PK2=None,
                       csv_GLstrain=None,
@@ -801,6 +893,10 @@ def visualize_results(input_file, average, num_domains,
     if plot_stress_norms:
         plot_stress_norm(cauchy, symm, m, nqp, nel, ninc, times, plot_stress_norms)
 
+    # Plot better stress norms
+    if plot_better_stress_norms:
+        plot_better_stress_norm(PK2, SIGMA, M, E, Ecal, Gamma, nqp, nel, ninc, plot_better_stress_norms)
+
     # Output csvs
     #TODO: add an argument for specifying what time to gather statistics on
     if csv_cauchy:
@@ -909,6 +1005,11 @@ def get_parser():
         help="Optional filename to plot norms of cauchy stress, symmetric micro stress,\
               difference between Cauchy and symmetric micro stresses, and higher order\
               stress.")
+    parser.add_argument('--plot-better-stress-norms', type=str, required=False, default=None,
+        help="Optional filename to plot norms of PK2 stress, Symmetric micro stress,\
+              difference between PK2 and Symmetric micro stresses, and higher order\
+              stress, all against norms of Green-Lagrange strain, Micro strain,\
+              and Micro-deformation.")
     parser.add_argument('--csv-cauchy', type=str, required=False, default=None,
         help="Optional filename for csv output of Cauchy stress summary statistics")
     parser.add_argument('--csv-PK2', type=str, required=False, default=None,
@@ -966,6 +1067,7 @@ if __name__ == '__main__':
                 plot_rotation_diff=args.plot_rotation_diff,
                 plot_stretch_diff=args.plot_stretch_diff,
                 plot_stress_norms=args.plot_stress_norms,
+                plot_better_stress_norms=args.plot_better_stress_norms,
                 csv_cauchy=args.csv_cauchy,
                 csv_PK2=args.csv_PK2,
                 csv_GLstrain=args.csv_GLstrain,
@@ -980,4 +1082,4 @@ if __name__ == '__main__':
                 csv_all_quantities_single_domain=args.csv_all_quantities_single_domain,
                 rho_binder=args.rho_binder,
                 rho_grain=args.rho_grain,
-                ))
+                ))                         
