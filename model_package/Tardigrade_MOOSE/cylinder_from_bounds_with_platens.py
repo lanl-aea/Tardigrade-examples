44414c3d4e150c4da8e1baf39c515a945eb325be
Thomas Ernest Allard <tea@lanl.gov>
2024-11-14 14:17:27 -0700

    FEAT: get cylinder from bounds + platens mesh scripting working

--- model_package/Tardigrade_MOOSE/cylinder_from_bounds_with_platens.py
+++ model_package/Tardigrade_MOOSE/cylinder_from_bounds_with_platens.py
@@ -9,7 +9,7 @@ import numpy
 import subprocess
 
 
-def mesh(rad, height, x0, y0, z0, seed_size, output_file, platen_rad_factor, platen_depth):
+def mesh(rad, height, x0, y0, z0, seed_size, output_file, platen_rad_factor, platen_depth, gap=None):
     ''' Mesh a cylinder using Cubit with platens
 
     :param float rad: Cylinder radius
@@ -44,30 +44,60 @@ def mesh(rad, height, x0, y0, z0, seed_size, output_file, platen_rad_factor, pla
     cubit.cmd('sideset 4 add surface 58 50 62 54')
     cubit.cmd('sideset 4 name "y_plane"')
 
-    # Platens
+    # Top Platen
     cubit.cmd(f'create cylinder height {platen_depth} radius {rad*platen_rad_factor}')
     cubit.cmd('webcut volume 9 with plane xplane offset 0')
     cubit.cmd('webcut volume 9 10 with plane yplane offset 0')
-    cubit.cmd('move Volume 9 10 11 12 x 0 y 0 z 3.0 include_merged')
+    cubit.cmd(f'move Volume 9 10 11 12 x 0 y 0 z {0.5*(height + platen_depth)} include_merged')
 
-    # Mesh and move
+    # Bottom Platen
+    cubit.cmd(f'create cylinder height {platen_depth} radius {rad*platen_rad_factor}')
+    cubit.cmd('webcut volume 13 with plane xplane offset 0')
+    cubit.cmd('webcut volume 13 14 with plane yplane offset 0')
+    cubit.cmd(f'move Volume 13 14 15 16 x 0 y 0 z {-0.5*(height + platen_depth)} include_merged')
+
+    # Partition by using specimen surface
+    cubit.cmd(f'webcut volume 9 10 11 12 13 14 15 16 with sheet extended from surface 39 45 63 53')
+
+    # Platen Side sets
+    cubit.cmd('sideset 5 add surface 128 135 140 143 150 153 158 165')
+    cubit.cmd('sideset 5 name "top_platen_bottom"')
+    cubit.cmd('sideset 6 add surface 130 133 138 145 148 155 160 163')
+    cubit.cmd('sideset 6 name "top_platen_top"')
+    cubit.cmd('sideset 7 add surface 168 175 180 183 190 193 198 205')
+    cubit.cmd('sideset 7 name "bottom_platen_bottom"')
+    cubit.cmd('sideset 8 add surface 170 173 178 185 188 195 200 203')
+    cubit.cmd('sideset 8 name "bottom_platen_top"')
+
+    # Mesh and Move
+    cubit.cmd('imprint volume 1 2 3 4 5 6 7 8')
+    cubit.cmd('merge volume 1 2 3 4 5 6 7 8')
+    cubit.cmd(f'volume 1 2 3 4 5 6 7 8 size {seed_size}')
+    cubit.cmd('mesh volume 1 2 3 4 5 6 7 8')
     cubit.cmd('imprint volume all')
     cubit.cmd('merge volume all')
-    cubit.cmd(f'volume all size {seed_size}')
-    cubit.cmd('mesh volume all')
+    cubit.cmd(f'volume 9 10 11 12 17 18 19 20 13 14 15 16 21 22 23 24 size {seed_size}')
+    cubit.cmd('mesh volume 9 10 11 12 17 18 19 20 13 14 15 16 21 22 23 24')
     cubit.cmd(f'move Volume all x {x0} y {y0} z {z0} include_merged')
-    # Make a new block for all elements to export
-    cubit.cmd('block 9 add hex all')
-    cubit.cmd('block 9 name "all"')
+    if gap:
+        cubit.cmd(f'move Volume 9 10 11 12 17 18 19 20 x 0 y 0 z {gap} include_merged')
+    # Blocks
+    cubit.cmd('block 1 add volume 1 2 3 4 5 6 7 8')
+    cubit.cmd('block 1 name "specimen"')
+    cubit.cmd('block 2 add volume 9 10 11 12 17 18 19 20')
+    cubit.cmd('block 2 name "top_platen"')
+    cubit.cmd('block 3 add volume 13 14 15 16 21 22 23 24')
+    cubit.cmd('block 3 name "bottom_platen"')
 
     # Export
-    cubit.cmd(f'export mesh "{output_file}.e" block 9 overwrite')
+    #cubit.cmd(f'
+    cubit.cmd(f'export mesh "{output_file}.e" block 1 2 3 overwrite')
 
 
     return 0
 
 
-def cylinder_from_bounds_with_platens(output_file, bounds_file, seed_size, platen_rad_factor=1.5, platen_depth=1.0, xdmf=True, ascii=False):
+def cylinder_from_bounds_with_platens(output_file, bounds_file, seed_size, platen_rad_factor=1.5, platen_depth=1.0, xdmf=True, ascii=False, gap=None):
     '''Create a cylinder mesh from the bounds of a DNS file with platens
 
     :param str output_file: The output filename
@@ -102,7 +132,7 @@ def cylinder_from_bounds_with_platens(output_file, bounds_file, seed_size, plate
     z0 = zmin + (height / 2)
 
     # create mesh
-    mesh(rad, height, x0, y0, z0, seed_size, output_file, platen_rad_factor, platen_depth)
+    mesh(rad, height, x0, y0, z0, seed_size, output_file, platen_rad_factor, platen_depth, gap)
 
     # convert to XDMF with subprocess
     if xdmf:
@@ -135,6 +165,8 @@ def get_parser():
         help='The option to convert default exodus mesh to XDMF (binary)')
     parser.add_argument('--ascii', type=str, required=False, default=False,
         help='The option to convert binary XDMF mesh to ascii')
+    parser.add_argument('--gap', type=str, required=False, default=None,
+        help='An initial gap to place between specimen and platens')
     return parser
 
 
@@ -148,5 +180,6 @@ if __name__ == '__main__':
                                                platen_depth=args.platen_depth,
                                                xdmf=bool(args.xdmf),
                                                ascii=bool(args.ascii),
+                                               gap=args.gap,
                                                ))
 
