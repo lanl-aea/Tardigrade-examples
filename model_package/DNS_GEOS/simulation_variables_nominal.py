af1ab6a42b2c69d732f47488689bf4dbe92a0bba
Thomas Ernest Allard <tea@lanl.gov>
2024-11-19 13:38:51 -0700

    FEAT: update elastic_cylinder dict for GEOS verification study

--- model_package/DNS_GEOS/simulation_variables_nominal.py
+++ model_package/DNS_GEOS/simulation_variables_nominal.py
@@ -2,12 +2,11 @@ import copy
 
 elastic_cylinder = {
     # DNS parameters
-    'diam': 2.0,
-    'height': 2.0,
-    'material_E': 450.0,
-    'material_nu': 0.25,
-    'rho_binder': 1.860e-9,
-    'rho_grain': 2.001e-9,
+    'diam': 5.0,
+    'height': 5.0,
+    'material_E': 250.,
+    'material_nu': 0.2,
+    'rho_binder': 1.935e-9,
     'cut': True,
     # Mesh file root to copy if Cubit is not found
     'mesh_copy_root': 'GEOS_cylinder',
@@ -17,14 +16,25 @@ elastic_cylinder = {
     'filter_parallel': 8,
     # parameters for calibration
     'calibration_case': 3,
-    'calibration_increment': 5,
+    'calibration_increment': [5],
     'ignore_boundary': True,
     # paramters for Tardigrade-MOOSE
-    'macro_disp': 0.2729,
+    'macro_disp': 0.1250143659720484,
     'macro_duration': 1.0,
-    'macro_BC': 'clamp',
+    'macro_BC': 'slip',
 }
-# Location of DNS files in petalibrary
+# Handle DNS files for petalibrary copy
+root = 'GEOS-MPM/MiscellaneousSimulations/UniaxialCylinderCompression'
+levels = ['000000', '000023', '000045', '000067', '000089', '000111', '000133', '000155',
+          '000177', '000199', '000221', '000243', '000265', '000266']
+ranks = 8
+elastic_cylinder['DNS_files'] = {
+    'main': [f'{root}/vtkOutput.pvd'] + [f'{root}/reactionHistory.csv'],
+    'vtms': [f'{root}/vtkOutput/{level}.vtm' for level in levels]}
+for level in levels:
+    elastic_cylinder['DNS_files'][level] = [f'{root}/vtkOutput/{level}/particles/Level0/ParticleRegion1/rank_{i}.vtu' for i in range(0, ranks)]
+
+# After files have been copied to peta_data_copy/, only the following items are needed to run the workflow(s)
 elastic_cylinder['DNS_file'] = 'vtkOutput.pvd'
-elastic_cylinder['DNS_fileroot'] = '/projects/tea/PSAAP/GEOS_new_cylinder_from_Jay/'
-elastic_cylinder['DNS_forces'] = '.'
+elastic_cylinder['DNS_fileroot'] = 'GEOS_elastic_cylinder'
+elastic_cylinder['DNS_forces'] = 'reactionHistory.csv'
