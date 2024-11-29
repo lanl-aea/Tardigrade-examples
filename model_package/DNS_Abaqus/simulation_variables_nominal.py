750f205f1903747016e5dabc6446db780720ca5d
Thomas Ernest Allard <tea@lanl.gov>
2024-11-29 15:42:00 -0700

    MAINT: directly specify "calibration_increment" for case 1 calibration routines

--- model_package/DNS_Abaqus/simulation_variables_nominal.py
+++ model_package/DNS_Abaqus/simulation_variables_nominal.py
@@ -49,6 +49,7 @@ elastic_cylinder_clamp = {
     'filter_parallel': 8,
     # parameters for calibration
     'calibration_case': 1,
+    'calibration_increment': [1, 2, 3, 4, 5],
     'UQ_file': False,
     'ignore_boundary': True,
     # paramters for Tardigrade-MOOSE
