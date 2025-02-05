6feb9ddd8e47909b6821d719a23a2f70b2a970fe
Thomas Ernest Allard <tea@lanl.gov>
2025-02-05 11:36:16 -0700

    FEAT: add option to call the FilterMicroDomainDamage class if homogenizing damage

--- model_package/Filter/run_micromorphic_filter.py
+++ model_package/Filter/run_micromorphic_filter.py
@@ -6,7 +6,7 @@ import sys
 import micromorphic_filter.filter_dns
 
 
-def run_filter(config_file):
+def run_filter(config_file, damage_class=None):
     '''Run the Micromorphic Filter
 
     :param str config_file: The filter configuration file
@@ -14,7 +14,10 @@ def run_filter(config_file):
     Runs the Micromorphic Filter
     '''
 
-    f = micromorphic_filter.filter_dns.FilterMicroDomain(config_file)
+    if damage_class:
+        f = micromorphic_filter.filter_dns.FilterMicroDomainDamage(config_file)
+    else:
+        f = micromorphic_filter.filter_dns.FilterMicroDomain(config_file)
     f.filterIncrements()
 
     return 0
@@ -29,6 +32,8 @@ def get_parser():
     parser=argparse.ArgumentParser(description=cli_description, prog=prog)
     parser.add_argument('--config-file', type=str, required=True,
         help='Specify the filter configuration file')
+    parser.add_argument('--damage-class', type=str, required=False, default=False,
+        help='Flag to request the FilterMicroDomainDamage filter class')
     return parser
 
 
@@ -36,4 +41,6 @@ if __name__ == '__main__':
     parser = get_parser()
 
     args = parser.parse_args()
-    sys.exit(run_filter(config_file=args.config_file))
+    sys.exit(run_filter(config_file=args.config_file,
+                        damage_class=args.damage_class,
+                        ))
