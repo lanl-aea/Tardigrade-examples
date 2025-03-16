c0394b71ff59598e3afea321953b2837873535ee
Thomas Ernest Allard <tea@lanl.gov>
2025-03-16 17:21:55 -0600

    MAINT: add options to specify final location of specimen and flag to export platen meshes

--- model_package/Tardigrade_MOOSE/brazilian_disk_apparatus_symmetry.py
+++ model_package/Tardigrade_MOOSE/brazilian_disk_apparatus_symmetry.py
@@ -7,10 +7,11 @@ import sys
 import cubit
 import numpy
 
+from misc_utilities import str2bool
 
 def brazilian_disk_apparatus(output_file, specimen_seed_size, platen_seed_size,
                              height, width, chord, app_rad, app_dep, spec_rad, spec_dep, tol,
-                             symmetry='eighth'):
+                             symmetry='eighth', x0=0., y0=0., z0=0., export_platens=True):
     '''Create a Brazilian Disk specimen and loading apparatus
 
     :param str output_file: The output filename
@@ -26,14 +27,19 @@ def brazilian_disk_apparatus(output_file, specimen_seed_size, platen_seed_size,
     :param float tol: A tolerance / gap distance to insert between Brazilian disk \
                       compression specimen and platens
     :param str symmetry: Type of symmetry to create, either "eighth" or "quarter"
+    :param float x0: The x-location to move geometry for the center of the Brazil Disk'
+    :param float y0: The y-location to move geometry for the center of the Brazil Disk
+    :param float z0: The z-location to move geometry for the center of the Brazil Dis
+    :param bool export_platens: Flag to export platen meshes of the brazilian disk apparatus
 
-    :returns: ``output_file``
+    :returns: Write ``output_file``.cub, ``output_file``_specimen.inp, and optionally ``output_file``_bottom_platen.inp
     '''
 
     perp_dist = app_rad - numpy.sqrt((app_rad**2 - (0.5*chord)**2))
     print(perp_dist)
     bottom_spec = height - perp_dist
     gap = 2*(spec_rad - perp_dist)
+    offset = (app_dep - spec_dep)/2
 
     width_2 = (width - chord)/2.
 
@@ -69,6 +75,12 @@ def brazilian_disk_apparatus(output_file, specimen_seed_size, platen_seed_size,
         cubit.cmd('delete volume 2 4 5 6 7 8 11 12')
     else:
         print('Specify a valid type of symmetry!')
+
+    # Move
+    move_x = width/2
+    move_y = bottom_spec+spec_rad+tol
+    move_z = offset + spec_dep/2
+    cubit.cmd(f'move Volume all x {x0 - move_x} y {y0 - move_y} z {z0 - move_z} include_merged')
 
     # Blocks and sets
     if symmetry == 'eighth':
@@ -153,9 +165,9 @@ def brazilian_disk_apparatus(output_file, specimen_seed_size, platen_seed_size,
     # Output
     cubit.cmd(f'save as "{output_file}.cub" overwrite')
     cubit.cmd(f'export mesh "{output_file}.e"  overwrite')
-    cubit.cmd(f'export abaqus "{output_file}_bottom_platen.inp" block 1 source_csys 0 target_csys 0 partial dimension 3 overwrite')
+    if export_platens == True:
+        cubit.cmd(f'export abaqus "{output_file}_bottom_platen.inp" block 1 source_csys 0 target_csys 0 partial dimension 3 overwrite')
     cubit.cmd(f'export abaqus "{output_file}_specimen.inp" block 2 source_csys 0 target_csys 0 partial dimension 3 overwrite')
-    #cubit.cmd(f'export abaqus "{output_file}.inp" instance_per_block source_csys 0 target_csys 0 partial dimension 3 overwrite  everything')
 
     return
 
@@ -193,6 +205,14 @@ def get_parser():
               compression specimen and platens')
     parser.add_argument('--symmetry', type=str, required=False, default='eighth',
         help='Type of symmetry to create, either "eighth" or "quarter"')
+    parser.add_argument('--x0', type=float, required=False, default=0.,
+        help='The x-location to move geometry for the center of the Brazil Disk')
+    parser.add_argument('--y0', type=float, required=False, default=0.,
+        help='The y-location to move geometry for the center of the Brazil Disk')
+    parser.add_argument('--z0', type=float, required=False, default=0.,
+        help='The z-location to move geometry for the center of the Brazil Disk')
+    parser.add_argument('--export-platens', type=str, required=False, default='True',
+        help='Flag to export platen meshes of the brazilian disk apparatus')
 
     return parser
 
@@ -212,4 +232,8 @@ if __name__ == '__main__':
                                       spec_dep=args.spec_dep,
                                       tol=args.tol,
                                       symmetry=args.symmetry,
+                                      x0=args.x0,
+                                      y0=args.y0,
+                                      z0=args.z0,
+                                      export_platens=str2bool(args.export_platens),
                                       ))
