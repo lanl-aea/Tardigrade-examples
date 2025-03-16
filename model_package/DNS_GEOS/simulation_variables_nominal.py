47d41b5f5651b9cbeeae0e9566a95accdf05eb2d
Thomas Ernest Allard <tea@lanl.gov>
2025-03-16 12:00:38 -0600

    MAINT: add simulation variables for GOES I43.01 study

--- model_package/DNS_GEOS/simulation_variables_nominal.py
+++ model_package/DNS_GEOS/simulation_variables_nominal.py
@@ -38,3 +38,31 @@ for level in levels:
 elastic_cylinder['DNS_file'] = 'vtkOutput.pvd'
 elastic_cylinder['DNS_fileroot'] = 'GEOS_elastic_cylinder'
 elastic_cylinder['DNS_forces'] = 'reactionHistory.csv'
+
+I43_01 = {
+    # DNS parameters
+    'diam': 5.0501088,
+    'height': 4.6736,
+    'material_E': 250.,
+    'material_nu': 0.2,
+    'rho_binder': 1.935e-9,
+    'cut': True,
+    # Mesh file root to copy if Cubit is not found
+    'mesh_copy_root': 'GEOS_cylinder',
+    # parameters for micromorphic filter
+    'acceleration': False,
+    'velocity': False,
+    'filter_parallel': 8,
+    # parameters for calibration
+    'calibration_case': 3,
+    'calibration_increment': [5],
+    'ignore_boundary': True,
+    # paramters for Tardigrade-MOOSE
+    'macro_disp': 0.1250143659720484,
+    'macro_duration': 1.0,
+    'macro_BC': 'slip',
+}
+
+I43_01['DNS_fileroot'] = 'GEOS_2025-03-02_I43-01_take2'
+I43_01['DNS_file'] = 'vtkOutput_good.pvd'
+I43_01['DNS_forces'] = 'reactionHistory.csv'
