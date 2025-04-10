1dcc89a0c3d7e3de9bcc4e1fb685e74414c06909
Thomas Ernest Allard <tea@lanl.gov>
2025-04-10 11:40:55 -0600

    MAINT: just use strings to handle damage class for now

--- model_package/Filter/run_micromorphic_filter.py
+++ model_package/Filter/run_micromorphic_filter.py
@@ -1,7 +1,13 @@
 #!python
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '8'
+os.environ['OMP_NUM_THREADS'] = '8'
+os.environ['MKL_NUM_THREADS'] = '8'
+
 import argparse
 import pathlib
 import sys
+import time
 
 import micromorphic_filter.filter_dns
 
@@ -14,11 +20,15 @@ def run_filter(config_file, damage_class=None):
     Runs the Micromorphic Filter
     '''
 
-    if damage_class:
+    if (damage_class is not None) and (damage_class.lower() == 'true'):
         f = micromorphic_filter.filter_dns.FilterMicroDomainDamage(config_file)
     else:
         f = micromorphic_filter.filter_dns.FilterMicroDomain(config_file)
+
+    start_time = time.time()
     f.filterIncrements()
+    end_time = time.time()
+    print(f'finished executing in {end_time - start_time} seconds!')
 
     return 0
 
@@ -32,7 +42,7 @@ def get_parser():
     parser=argparse.ArgumentParser(description=cli_description, prog=prog)
     parser.add_argument('--config-file', type=str, required=True,
         help='Specify the filter configuration file')
-    parser.add_argument('--damage-class', type=str, required=False, default=False,
+    parser.add_argument('--damage-class', type=str, required=False, default=None,
         help='Flag to request the FilterMicroDomainDamage filter class')
     return parser
 
