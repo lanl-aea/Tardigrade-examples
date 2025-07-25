0ea32176bc816d76586024fd7739d10ed1eaf8be
Thomas Ernest Allard <thomas.allard@colorado.edu>
2025-07-25 11:16:41 -0600

    FEAT: handle selective transfer of large GEOS DNS

--- model_package/peta.py
+++ model_package/peta.py
@@ -6,6 +6,8 @@ import pathlib
 import argparse
 import subprocess
 
+import xml.etree.ElementTree as ET
+
 from model_package.DNS_Ratel import simulation_variables_nominal as Ratel_variables
 from model_package.DNS_GEOS import simulation_variables_nominal as GEOS_variables
 
@@ -17,7 +19,6 @@ def transfer_Ratel_files(source_files, source_directory, output_directory, usern
     :params str source_directory: The common CU Peta Library DNS results path
     :params str output_directory: The destination directory of DNS files
     :params str username: The user's CU identikey
-
     '''
 
     # Create output directory if it doesn't exist
@@ -55,7 +56,6 @@ def transfer_GEOS_files(source_files_dict, source_directory, output_directory, s
     :params str output_directory: The root destination directory of DNS files
     :params str split_key: a unique string to split the full Petalibrary path for copying
     :params str username: The user's CU identikey
-
     '''
 
     # Create output directory if it doesn't exist
@@ -66,7 +66,7 @@ def transfer_GEOS_files(source_files_dict, source_directory, output_directory, s
     for key in source_files_dict.keys():
         source_files = source_files_dict[key]
 
-        # Hand destination directory
+        # Handle destination directory
         dest_split = f"{source_files[0].split(split_key)[-1]}"
         if len(dest_split.split('/')) == 1:
             dest_dir = f"{output_directory}/."
@@ -94,6 +94,95 @@ def transfer_GEOS_files(source_files_dict, source_directory, output_directory, s
             p = subprocess.Popen(full_command)
             os.waitpid(p.pid, 0)
 
+    return 0
+
+
+def transfer_GEOS_files_selective(source_files_dict, source_directory, output_directory, split_key, username):
+    '''Transfer GEOS DNS ascii VTK results and subdirectories a series of scp commands (multiple sources, multiple destinations)
+    for a specific selection of timesteps and modify associated XML data of PVD and VTM files
+
+    :params dict source_files_dict: The a dictionary containing lists of files GEOS ascii VTK results to copy
+    :params str source_directory: The common CU Peta Library DNS results path
+    :params str output_directory: The root destination directory of DNS files
+    :params str split_key: a unique string to split the full Petalibrary path for copying
+    :params str username: The user's CU identikey
+    '''
+
+    # Create output directory if it doesn't exist
+    if os.path.exists(output_directory) == False:
+        os.makedirs(output_directory)
+
+    # Loop over the individual keys to copy files
+    for key in source_files_dict.keys():
+        source_files = source_files_dict[key]
+
+        # Handle destination directory
+        dest_split = f"{source_files[0].split(split_key)[-1]}"
+        if len(dest_split.split('/')) == 1:
+            dest_dir = f"{output_directory}/."
+        else:
+            dest_dir = f"{output_directory}/{'/'.join(dest_split.split('/')[0:-1])}/."
+        if os.path.exists(dest_dir.split('.')[0]) == False:
+            os.makedirs(dest_dir.split('.')[0])
+
+        # Build single command to copy all relevant files at once
+        sources = []
+        for file in source_files:
+            dest_file = f"{output_directory}/{file.split(split_key)[-1]}"
+            if os.path.isfile(dest_file) == False:
+                sources.append(f'{source_directory}/{file}')
+
+        # Only initiate file transfer if there are new files to copy
+        if len(sources) == 0:
+            continue
+        else:
+            print(sources[0])
+            # Transfer files
+            if len(sources) == 1:
+                file_string = f"{username}@login.rc.colorado.edu:{sources[0]}"
+            else:
+                file_string = f"{username}@login.rc.colorado.edu:" + '{' + f"{','.join(sources)}" + '}'
+            full_command = ["scp", "-P", "22", file_string, dest_dir]
+            p = subprocess.Popen(full_command)
+            os.waitpid(p.pid, 0)
+
+        # Trim main pvd file to remove unnecessary timesteps
+        if key == 'main':
+            print(source_files)
+            # Find the target vtm files to keep in pvd file
+            target_files = [file.split('/')[-1] for file in source_files_dict['vtms']]
+            # parse the XML
+            pvd_file = f"{output_directory}/{source_files[0].split('/')[-1]}"
+            tree = ET.parse(pvd_file)
+            root=tree.getroot()
+            # Find collection element
+            collection=root.find('Collection')
+            # Remove all DataSet elements not in target_files
+            for dataset in list(collection):
+                vtm_file = dataset.attrib['file'].split('/')[-1]
+                print(vtm_file)
+                if vtm_file not in target_files:
+                    collection.remove(dataset)
+                else:
+                    dataset.set('file', f'{vtm_file.split('.')[0]}_trimmed.vtm')
+            # Write the filtered xml file
+            tree.write(f"{output_directory}/vtkOutput_trimmed.pvd", encoding='utf-8', xml_declaration=True)
+
+        # Trim vtm files to remove backgroundGrid data
+        if key == 'vtms':
+            for vtm_file in source_files:
+                vtm_filename = vtm_file[0].split('/')[-1]
+                vtm_output_name = f"{output_directory}/vtkOutput/{vtm_filename.split('.')}_trimmed.vtm"
+                vtm = f"{output_directory}/vtkOutput/{vtm_filename}"
+                tree = ET.parse(vtm)
+                root = tree.getroot()
+                # Remove all backgroundGrid
+                for multiblock in root.finall(".//vtkMultiBlockDataSet"):
+                    for block in list(multiblock):
+                        if block.tag == "Block" and block.attrib.get("name") == "backgroundGrid":
+                            multiblock.remove(block)
+                # Write the filtered xml file
+                tree.write(vtm_output_name, encoding='utf-8', xml_declaration=True)
 
     return 0
 
@@ -128,7 +217,7 @@ def peta_copy(source_directory, output_directory):
     MAP_directory = output_directory / "Ratel_I43_MAP"
     transfer_Ratel_files(source_files, source_directory, MAP_directory, username)
 
-    # GEOS transfer
+    # GEOS transfer for elastic cylinder
     source_files_dicts = (('GEOS_elastic_cylinder', 'UniaxialCylinderCompression/', GEOS_variables.elastic_cylinder['DNS_files']),
                            )
     for top_directory, split_key, source_files_dict in source_files_dicts:
@@ -136,6 +225,13 @@ def peta_copy(source_directory, output_directory):
         print(GEOS_output_directory)
         transfer_GEOS_files(source_files_dict, source_directory, GEOS_output_directory, split_key, username)
 
+    # GEOS transfer for larger DNS
+    source_files_dicts = (('GEOS_I43_01_sim38', 'lowRes_ceramicPuck_I43p01_sim38/', GEOS_variables.I43_01_sim38['DNS_files']),
+                           )
+    for top_directory, split_key, source_files_dict in source_files_dicts:
+        GEOS_output_directory = f'{output_directory}/{top_directory}'
+        print(GEOS_output_directory)
+        transfer_GEOS_files_selective(source_files_dict, source_directory, GEOS_output_directory, split_key, username)
 
     return 0
 
